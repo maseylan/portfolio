59ae9ab17db9d60bc4d0328bd3b33c58b429b849
maseylan <rafliahmad758@gmail.com>
2025-05-25 15:46:45 +0700

    static images solving

--- src/app/resources/content.js
+++ src/app/resources/content.js
@@ -115,7 +115,7 @@ const about = {
         images: [
           // optional: leave the array empty if you don't want to display images
           {
-            src: "/images/projects/project-01/porto.jpg",
+            src: "/images/projects/project-01/image-04.jpg",
             alt: "Once UI Project",
             width: 16,
             height: 9,
@@ -230,7 +230,7 @@ const about = {
         // optional: leave the array empty if you don't want to display images
         images: [
           {
-            src: "/images/projects/project-01/porto.jpg",
+            src: "/images/projects/project-01/image-04.jpg",
             alt: "Project image",
             width: 16,
             height: 9,
